0fe4c7f91491df5870132968fa1888d231bca731
nzlefko <nzlefko@gmail.com>
2025-07-16 11:43:05 +0300

    Chromdriver path update

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 @pytest.fixture
 def driver():
     # Dynamically build the path to chromedriver inside the project
-    driver_path = os.path.join(os.getcwd(), "drivers", "chromedriver")
+    driver_path = os.path.join(os.getcwd(), "drivers", "chromedriver","chromedriver-mac-x64", "chromedriver")
     service = ChromeService(executable_path=driver_path)
 
     # Initialize WebDriver with custom service

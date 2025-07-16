64a70affa35c78041209da77c3f947a9837977f1
nzlefko <nzlefko@gmail.com>
2025-07-16 17:00:21 +0300

    add 2 api tests

--- tests/api/get/test_get_all_products_brands.py
+++ tests/api/get/test_get_all_products_brands.py
@@ -1,9 +1,14 @@
 import requests
 from tests.api.config.api_constants import BASE_URL
 
-
 def test_get_all_products():
     response = requests.get(f"{BASE_URL}/productsList")
 
     assert response.status_code == 200
     assert "products" in response.json()
+
+def test_get_all_brands():
+    response = requests.get(f"{BASE_URL}/brandsList")
+
+    assert response.status_code == 200
+    assert "brands" in response.json()
